8f7e7273927be422a50506d530fc036a45e15e92
xuwei-k <6b656e6a69@gmail.com>
2012-09-17 01:04:44 +0900

    tab completions

--- src/main/scala/scalag/ScalagPlugin.scala
+++ src/main/scala/scalag/ScalagPlugin.scala
@@ -2,6 +2,8 @@ package scalag
 
 import sbt._
 import sbt.Keys._
+import sbt.complete.Parser
+import sbt.complete.DefaultParsers._
 
 /**
  * Scalag xsbt plugin
@@ -18,11 +20,11 @@ import sbt.Keys._
  */
 object ScalagPlugin extends Plugin {
 
-  val scalagTask = inputTask {
-    (task: TaskKey[Seq[String]]) =>
+  val scalagTask = InputTask(_ => scalagParser) {
+    task =>
       (task, scalaSource in Compile, scalaSource in Test,
         resourceDirectory in Compile, resourceDirectory in Test) map {
-          case (args, srcDir, testDir, resourceDir, testResourceDir) =>
+          case ((n, args), srcDir, testDir, resourceDir, testResourceDir) =>
             if (!isFrozen) {
               freeze()
             }
@@ -32,24 +34,33 @@ object ScalagPlugin extends Plugin {
               resourceDir = resourceDir,
               testResourceDir = testResourceDir
             )
-            operations.apply(ScalagInput(args, settings))
+            (operations.map { _.operation }.reduceLeft { _ orElse _ } orElse defaultOperation)(ScalagInput(n +: args.mkString.split(" ").toList, settings))
         }
   }
 
-  lazy val g = InputKey[Unit]("g")
+  private lazy val scalagParser = {
+    val operationNames = operations.map { _.help.namespace }
+    Space ~> operationNames.tail.foldLeft(token(operationNames.head)) { _ | _ } ~ ((Space ?) ~> (any *))
+  }
+
   lazy val generate = InputKey[Unit]("generate")
+  lazy val g = InputKey[Unit]("g")
 
   val scalagSettings = inConfig(Compile)(Seq(
     g <<= scalagTask,
     generate <<= scalagTask
   ))
 
+  private[this] val defaultOperation: ScalagOperation = {
+    case ScalagInput(Nil, _) => showHelp()
+  }
+
   /**
    * Scalag command operations
    */
-  private[this] var operations: ScalagOperation = {
-    case ScalagInput(Nil, _) => showHelp()
-  }
+  private[this] var operations: Seq[ScalagCommand] = Nil
+
+  private def reducedOp = operations.map { _.operation }.reduceLeft { _ orElse _ }
 
   /**
    * Is already frozen?
@@ -74,33 +85,15 @@ object ScalagPlugin extends Plugin {
    * @param command scalag command
    */
   def addCommand(command: ScalagCommand): Unit = {
-    Option(command).foreach(command =>
-      addCommand(
-        operation = command.operation,
-        namespace = command.help.namespace,
-        args = command.help.args,
-        description = command.help.description
-      )
-    )
-  }
-
-  /**
-   * Add new command to Scalag
-   * @param operation scalag command operation
-   * @param namespace scalag help namespace
-   * @param description scalag help description
-   */
-  def addCommand(operation: ScalagOperation, namespace: String = "", args: Seq[String] = Nil, description: String = ""): Unit = {
-    this.synchronized {
-      if (isFrozen) {
-        throw new ScalagStateException("Scalag is already frozen. You cannnot add commands any more.")
-      } else {
-        val help = ScalagHelp(namespace, args, description)
-        if (namespace != "") {
-          helps ++= help.toString
-        }
-        operations = operations orElse operation orElse {
-          case ScalagInput(n :: _, _) if n == namespace => help.showUsage()
+    Option(command).foreach { command =>
+      this.synchronized {
+        if (isFrozen) {
+          throw new ScalagStateException("Scalag is already frozen. You cannnot add commands any more.")
+        } else {
+          if (command.help.namespace != "") {
+            helps ++= command.help.toString
+          }
+          operations :+= command
         }
       }
     }
@@ -111,9 +104,12 @@ object ScalagPlugin extends Plugin {
    */
   def freeze(): Unit = {
     this.synchronized {
-      operations = operations orElse {
+      operations
+      /* TODO
+      {
         case _ => showHelp()
       }
+      */
       isFrozen = true
     }
   }
